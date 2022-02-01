97034dfcdebba7bce3f0bd4a3856101128a4a084
tunakan27 <takumi.naito27@gmail.com>
2022-02-01 17:59:17 +0900

    [add] publicフォルダの読み込みを行う

--- app.js
+++ app.js
@@ -4,11 +4,8 @@ const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
 
+app.use(express.static('./public'))
 app.use(express.json())
-
-app.get('/hello', (req, res)=>{
-  res.send('Task Manager App')
-})
 
 app.use('/api/v1/tasks', tasks)
 
